70007e396b6f960dd3df64b4622fbf364c6bf21e
James <tikx.batera@gmail.com>
2021-04-21 10:28:22 -0300

    Changing database migrations to Postgresql.

--- blog/urls.py
+++ blog/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('posts.urls')),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    # path('__debug__/', include(debug_toolbar.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
